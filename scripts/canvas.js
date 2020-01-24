b0c517fc5f1cc43ccf40583cffa6bae9735e8a8c
Justin Hove <59522163+hovej@users.noreply.github.com>
2020-01-23 18:07:10 -0600

    Update canvas.js
    End game if player and enemy collide or if enemy reaches edge of game area

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -79,6 +79,9 @@ function updateGameArea() {
   for (let i=0; i<enemies.length; i++) {
     ctx.fillStyle = "red";
     ctx.fillRect(enemies[i].x, enemies[i].y, 20, 20);
+    if (collisionCheck(player, enemies[i] || enemies[i].x <= 0) {
+      endGame();
+    }
   }
   for (let i=0; i<missiles.length; i++) {
     ctx.fillStyle = "black";

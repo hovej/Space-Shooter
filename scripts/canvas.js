3a548583b813b4d87ac831838564fe04f44b5082
Justin Hove <59522163+hovej@users.noreply.github.com>
2020-01-27 13:16:22 -0600

    Remove levels array to move to new file

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -13,33 +13,6 @@ let score;
 let spawnSpeed;
 let killCount = 0;
 let currentLevel = 0;
-let levels = [
-  {
-    level: 1,
-    killReq: 5,
-    spawnTime: 3000
-  },
-  {
-    level: 2,
-    killReq: 8,
-    spawnTime: 2600
-  },
-  {
-    level: 3,
-    killReq: 10,
-    spawnTime: 2400
-  },
-  {
-    level: 4,
-    killReq: 15,
-    spawnTime: 2000
-  },
-  {
-    level: 5,
-    spawnTime: 1000,
-    killReq: 5
-  }
-]
 $(document).ready(canvas.focus());
 
 window.addEventListener('keyup', function(event) {

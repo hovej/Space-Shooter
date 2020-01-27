f2126922af6475f3f8bebebe702684eb5f01704d
Justin Hove <59522163+hovej@users.noreply.github.com>
2020-01-27 13:17:09 -0600

    Create levels.js
    Clearing space from canvas.js

---
+++ scripts/levels.js
@@ -0,0 +1,27 @@
+var levels = [
+  {
+    level: 1,
+    killReq: 5,
+    spawnTime: 3000
+  },
+  {
+    level: 2,
+    killReq: 8,
+    spawnTime: 2600
+  },
+  {
+    level: 3,
+    killReq: 10,
+    spawnTime: 2400
+  },
+  {
+    level: 4,
+    killReq: 15,
+    spawnTime: 2000
+  },
+  {
+    level: 5,
+    spawnTime: 1000,
+    killReq: 5
+  }
+]

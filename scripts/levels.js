1a6236cd89730297403b5c640daf11e1020d5f0c
Justin Hove <59522163+hovej@users.noreply.github.com>
2020-01-27 13:36:16 -0600

    Add levels 6-10

--- scripts/levels.js
+++ scripts/levels.js
@@ -6,7 +6,7 @@ var levels = [
   },
   {
     level: 2,
-    killReq: 8,
+    killReq: 5,
     spawnTime: 2600
   },
   {
@@ -16,12 +16,37 @@ var levels = [
   },
   {
     level: 4,
-    killReq: 15,
+    killReq: 10,
     spawnTime: 2000
   },
   {
     level: 5,
     spawnTime: 1000,
-    killReq: 5
+    killReq: 15
+  },
+  {
+    level: 6,
+    spawnTime: 800,
+    killReq: 20
+  },
+  {
+    level: 7,
+    spawnTime: 700,
+    killReq: 20
+  },
+  {
+    level: 8,
+    spawnTime: 600,
+    killReq: 25
+  },
+  {
+    level: 9,
+    spawnTime: 500,
+    killReq: 25
+  },
+  {
+    level: 10,
+    spawnTime: 400,
+    killReq: 25
   }
 ]
